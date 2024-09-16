81e9145e0a79aba5429e5a713d3b0b1681a85a0f
Łukasz Musiał <lukasz.musial@musialny.dev>
2024-09-16 17:27:28 +0200

    Initialize Ktor based entrypoint

---
+++ Core/src/main/kotlin/dev/musialny/portfoliomanager/Application.kt
@@ -0,0 +1,12 @@
+package dev.musialny.portfoliomanager
+
+import dev.musialny.portfoliomanager.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
